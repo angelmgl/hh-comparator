c8bf7eef425152fa7c39a5eb7d70692854322ddd
Angel Alemany <angelemegeele@gmail.com>
2023-09-03 15:59:59 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <h1 className="text-center text-4xl font-bold">HH Comparator</h1>
+        </div>
+    );
+}
