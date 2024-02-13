352ab6c5571ed5dc93feadc25db0054a2b196d15
Angel Alemany <angelemegeele@gmail.com>
2024-02-13 00:11:22 -0300

    feat: reorder current properties on change

--- src/hooks/useComparator.js
+++ src/hooks/useComparator.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import { 
+import {
     getTheCheapestSale,
     getTheCheapestRent,
-    getTheMostBuilded, 
-    getTheMostLand, 
-    getTheMostOwned, 
-    getTheMostValuableRentPerM2Build, 
-    getTheMostValuableRentPerM2Own, 
-    getTheMostValuableRentPerM2Land, 
+    getTheMostBuilded,
+    getTheMostLand,
+    getTheMostOwned,
+    getTheMostValuableRentPerM2Build,
+    getTheMostValuableRentPerM2Own,
+    getTheMostValuableRentPerM2Land,
     getTheMostValuableSalePerM2Build,
     getTheMostValuableSalePerM2Own,
     getTheMostValuableSalePerM2Land,
@@ -19,19 +19,25 @@ export default function useComparator(properties, filters) {
         second: null,
         third: null,
         fourth: null,
-    }); 
+    });
     const [hasTwoOrMoreValues, setHasTwoOrMoreValues] = useState(false);
     const [cheapestSale, setCheapestSale] = useState(null);
     const [cheapestRent, setCheapestRent] = useState(null);
     const [mostBuilded, setMostBuilded] = useState(null);
     const [mostLand, setMostLand] = useState(null);
     const [mostOwned, setMostOwned] = useState(null);
-    const [mostValuableSalePerM2Own, setMostValuableSalePerM2Own] = useState(null);
-    const [mostValuableRentPerM2Own, setMostValuableRentPerM2Own] = useState(null);
-    const [mostValuableSalePerM2Build, setMostValuableSalePerM2Build] = useState(null);
-    const [mostValuableRentPerM2Build, setMostValuableRentPerM2Build] = useState(null);
-    const [mostValuableSalePerM2Land, setMostValuableSalePerM2Land] = useState(null);
-    const [mostValuableRentPerM2Land, setMostValuableRentPerM2Land] = useState(null);
+    const [mostValuableSalePerM2Own, setMostValuableSalePerM2Own] =
+        useState(null);
+    const [mostValuableRentPerM2Own, setMostValuableRentPerM2Own] =
+        useState(null);
+    const [mostValuableSalePerM2Build, setMostValuableSalePerM2Build] =
+        useState(null);
+    const [mostValuableRentPerM2Build, setMostValuableRentPerM2Build] =
+        useState(null);
+    const [mostValuableSalePerM2Land, setMostValuableSalePerM2Land] =
+        useState(null);
+    const [mostValuableRentPerM2Land, setMostValuableRentPerM2Land] =
+        useState(null);
 
     // efecto para recuperar las propiedades de la URL si existen
     useEffect(() => {
@@ -43,43 +49,129 @@ export default function useComparator(properties, filters) {
 
         // Crear un objeto temporal para almacenar los parámetros encontrados
         const params = {
-            first: searchParams.get('first'),
-            second: searchParams.get('second'),
-            third: searchParams.get('third'),
-            fourth: searchParams.get('fourth'),
+            first: searchParams.get("first"),
+            second: searchParams.get("second"),
+            third: searchParams.get("third"),
+            fourth: searchParams.get("fourth"),
         };
 
         // Actualizar el estado con los valores de los parámetros, si existen
         setTimeout(() => {
-            setCurrentProperties(prevState => ({
+            setCurrentProperties((prevState) => ({
                 ...prevState,
                 ...params,
             }));
-        }, 2000)
+        }, 2000);
     }, []);
 
     // efecto para calcular todos los valores destacados al cambiar las propiedades seleccionadas o los filtros
     useEffect(() => {
-        setCheapestSale(getTheCheapestSale(hasTwoOrMoreValues, properties, currentProperties, filters.operationType));
-        setCheapestRent(getTheCheapestRent(hasTwoOrMoreValues, properties, currentProperties, filters.operationType));
-        setMostBuilded(getTheMostBuilded(hasTwoOrMoreValues, properties, currentProperties))
-        setMostLand(getTheMostLand(hasTwoOrMoreValues, properties, currentProperties))
-        setMostOwned(getTheMostOwned(hasTwoOrMoreValues, properties, currentProperties))
-        setMostValuableSalePerM2Own(getTheMostValuableSalePerM2Own(hasTwoOrMoreValues, properties, currentProperties));
-        setMostValuableSalePerM2Build(getTheMostValuableSalePerM2Build(hasTwoOrMoreValues, properties, currentProperties));
-        setMostValuableSalePerM2Land(getTheMostValuableSalePerM2Land(hasTwoOrMoreValues, properties, currentProperties));
-        setMostValuableRentPerM2Own(getTheMostValuableRentPerM2Own(hasTwoOrMoreValues, properties, currentProperties));
-        setMostValuableRentPerM2Build(getTheMostValuableRentPerM2Build(hasTwoOrMoreValues, properties, currentProperties));
-        setMostValuableRentPerM2Land(getTheMostValuableRentPerM2Land(hasTwoOrMoreValues, properties, currentProperties));
-    }, [currentProperties, hasTwoOrMoreValues, properties, filters])
+        setCheapestSale(
+            getTheCheapestSale(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties,
+                filters.operationType
+            )
+        );
+        setCheapestRent(
+            getTheCheapestRent(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties,
+                filters.operationType
+            )
+        );
+        setMostBuilded(
+            getTheMostBuilded(hasTwoOrMoreValues, properties, currentProperties)
+        );
+        setMostLand(
+            getTheMostLand(hasTwoOrMoreValues, properties, currentProperties)
+        );
+        setMostOwned(
+            getTheMostOwned(hasTwoOrMoreValues, properties, currentProperties)
+        );
+        setMostValuableSalePerM2Own(
+            getTheMostValuableSalePerM2Own(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+        setMostValuableSalePerM2Build(
+            getTheMostValuableSalePerM2Build(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+        setMostValuableSalePerM2Land(
+            getTheMostValuableSalePerM2Land(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+        setMostValuableRentPerM2Own(
+            getTheMostValuableRentPerM2Own(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+        setMostValuableRentPerM2Build(
+            getTheMostValuableRentPerM2Build(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+        setMostValuableRentPerM2Land(
+            getTheMostValuableRentPerM2Land(
+                hasTwoOrMoreValues,
+                properties,
+                currentProperties
+            )
+        );
+    }, [currentProperties, hasTwoOrMoreValues, properties, filters]);
 
     // efecto para saber si hay 2 o más propiedades
     useEffect(() => {
         const values = Object.values(currentProperties);
-        const countNonNull = values.filter(value => value !== null).length;
+        const countNonNull = values.filter((value) => value !== null).length;
 
         setHasTwoOrMoreValues(countNonNull >= 2);
-    }, [currentProperties])
+    }, [currentProperties]);
+
+    // efecto para reordenar los currentProperties
+    useEffect(() => {
+        // Función para reordenar currentProperties
+        const reorderProperties = (properties) => {
+            const values = Object.values(properties);
+            const sortedValues = values
+                .filter((value) => value !== null)
+                .concat(values.filter((value) => value === null));
+
+            return {
+                first: sortedValues[0] || null,
+                second: sortedValues[1] || null,
+                third: sortedValues[2] || null,
+                fourth: sortedValues[3] || null,
+            };
+        };
+
+        // Calcular el nuevo estado reordenado
+        const newProperties = reorderProperties(currentProperties);
+
+        // Convertir los objetos a string para comparar
+        const currentAsString = JSON.stringify(currentProperties);
+        const newAsString = JSON.stringify(newProperties);
+
+        // Actualizar el estado solo si el reordenamiento cambia el objeto
+        if (currentAsString !== newAsString) {
+            setCurrentProperties(newProperties);
+        }
+    }, [currentProperties]);
 
     return {
         currentProperties,
@@ -95,6 +187,6 @@ export default function useComparator(properties, filters) {
         mostValuableRentPerM2Build,
         mostValuableSalePerM2Land,
         mostValuableRentPerM2Land,
-        hasTwoOrMoreValues
-    }
+        hasTwoOrMoreValues,
+    };
 }
