a4e0bbf9b7eddbb198a4b53446f512a465fc6101
Angel Alemany <angelemegeele@gmail.com>
2024-03-18 00:39:44 -0300

    feat: create html tooltip for mobile

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FiChevronsUp } from "react-icons/fi";
 
 function classNames(condition, classesIfTrue, defaultClasses = "") {
@@ -7,17 +8,29 @@ function classNames(condition, classesIfTrue, defaultClasses = "") {
 }
 
 export default function Field({children, condition, tooltip}) {
+    const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
     return (
+    <div className="relative">
+        {isTooltipVisible && (
+        <p 
+            onClick={() => setIsTooltipVisible(!isTooltipVisible)}
+            className="tooltip cursor-pointer absolute -top-16 p-2 rounded-sm bg-white shadow-md text-xs"
+        >
+            {condition ? tooltip : ""}
+        </p>
+        )}
         <p
+            onClick={() => setIsTooltipVisible(!isTooltipVisible)}
             className={classNames(
                 condition,
-                "bg-[#397F68] text-white rounded",
+                "bg-[#397F68] text-white rounded cursor-pointer",
                 "mt-2 px-2 py-1 flex items-center leading-4 text-sm single-line-text"
             )}
-            title={condition ? tooltip : ''}
         >
             { children }
             {condition && <FiChevronsUp className="mr-1" />}
         </p>
+    </div>
     );
 }
